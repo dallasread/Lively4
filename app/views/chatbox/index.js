403f28bb41e84622c032679f30141bac25725bf7
Dallas Read <dallas@excitecreative.ca>
2014-12-14 23:10:44 -0400

    Offline statemtn.

--- app/views/chatbox/index.js
+++ app/views/chatbox/index.js
@@ -6,7 +6,7 @@ export default Ember.View.extend({
 
 		if (this.session.contact && !this.session.contact.get('messages').get('length')) {
 			var message = this.store.createRecord('message', {
-				body: this.session.chatbox.get('initial_online_message'),
+				body: this.session.chatbox.get('online') ? this.session.chatbox.get('initial_online_message') : this.session.chatbox.get('initial_offline_message'),
 				agent: this.session.contact.agent,
 				from_agent: true
 			});
