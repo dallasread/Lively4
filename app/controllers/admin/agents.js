17dfa9dd51074fdcfaac6765c0eab17df4f85adf
Dallas Read <dallas@excitecreative.ca>
2014-12-15 20:31:44 -0400

    New agent controller.

---
+++ app/controllers/admin/agents.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.ArrayController.extend({
+	actions: {
+		createAgent: function() {
+			var agent = this.store.createRecord('agent', {
+				name: "",
+				email: ""
+			});
+			this.session.chatbox.get('agents').addObject(agent);
+		},
+		deleteAgent: function(agent) {
+			if (confirm("Are you sure you want to delete this agent?")) {
+				this.session.chatbox.get('agents').removeObject(agent);
+				this.session.chatbox.save();
+			}
+		}
+	}
+});
