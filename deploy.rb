7ebd4ef11160fef06b22f963428c1e3d5388b0bb
Dallas Read <dallas@excitecreative.ca>
2014-12-17 13:40:23 -0400

    Replace dist.

--- deploy.rb
+++ deploy.rb
@@ -6,6 +6,6 @@ ARGV[0] ||= 'production'
 `cat dist/assets/lcs.js >> dist/assets/vendor.js`
 `mv dist/assets/vendor.js dist/assets/lcs.js`
 `rm dist/assets/vendor.css`
-`cp -rf dist/assets assets`
-`rm -rf dist`
-`mv assets dist`
+# `cp -rf dist/assets assets`
+# `rm -rf dist`
+# `mv assets dist`
