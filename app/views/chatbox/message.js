c3a1ac98c8155f8bbd98da189d54ec921f744654
Dallas Read <dallas@excitecreative.ca>
2014-12-17 01:46:59 -0400

    Fix mark as read.

--- app/views/chatbox/message.js
+++ app/views/chatbox/message.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
+	didInsertElement: function() {
+			if (!this.get('content.read')) {
+				if (this.get('content.from_agent')) {
+					this.get('content').set('read', true);
+					this.get('session.contact').save();
+				}
+			}
+		}
 });
