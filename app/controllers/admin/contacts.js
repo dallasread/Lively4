d7e63c34a1b85a84d1fcc136aef911c9b11edea5
Dallas Read <dallas@excitecreative.ca>
2014-12-18 10:47:40 -0400

    On holiday!

--- app/controllers/admin/contacts.js
+++ app/controllers/admin/contacts.js
@@ -1,20 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  sortProperties: ['online', 'contact_last_seen'],
+  sortAscending: false,
 	showAll: function() {
 		return this.get('filter') === 'all';
 	}.property('filter'),
 	filter: 'assigned',
 	contacts: function() {
-		if (this.session.agent.get('admin') && this.get('filter') === 'all') {
-			return this.get('content');
-		} else {
-			var e = this;
-			return this.get('content').filter(function(contact) {
-				return contact.get('agent.id') === e.session.agent.id;
-			});
-		}
-	}.property('filter', 'session.agent.admin', 'content.@each.agent'),
+		var e = this;
+		var content = this.get('content');
+    var filter = this.get('filter');
+
+    return content.filter(function(contact) {
+			if (filter === 'all') {
+				return true;
+			} else {
+				return contact.get('agent.id') === e.get('session.agent.id');
+			}
+    });
+	}.property('content.@each', 'filter'),
 	actions: {
 		setFilter: function(filter) {
 			if (this.get('filter') !== filter) {
